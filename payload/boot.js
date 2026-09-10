c5d22099a386d848f8df2bdb94c41c8b45500446
MicamAmorim <96069299+MicamAmorim@users.noreply.github.com>
2026-09-10 18:20:02 -0300

    Load author and all-years practice improvements

--- payload/boot.js
+++ payload/boot.js
@@ -39,8 +39,8 @@
       html=new TextDecoder().decode(window.pako.ungzip(bytes));
     }
 
-    if(!html || !/<html[\s>]/i.test(html)) throw new Error('HTML reconstruído é inválido.');
-    html=html.replace('</body>','<script src="/canonical-map-2009-2016.js?v=20260910-1"></'+'script><script src="/canonical-map-2017-2025.js?v=20260910-1"></'+'script><script src="/geometry-review.js?v=20260910-3"></'+'script><script src="/geometry-hotfix.js?v=20260910-2"></'+'script><script src="/canonical-bank.js?v=20260910-1"></'+'script><script src="/canonical-integrity.js?v=20260910-1"></'+'script></body>');
+    if(!html || !/<html[\\s>]/i.test(html)) throw new Error('HTML reconstruído é inválido.');
+    html=html.replace('</body>','<script src="/canonical-map-2009-2016.js?v=20260910-1"></'+'script><script src="/canonical-map-2017-2025.js?v=20260910-1"></'+'script><script src="/geometry-review.js?v=20260910-3"></'+'script><script src="/geometry-hotfix.js?v=20260910-2"></'+'script><script src="/canonical-bank.js?v=20260910-1"></'+'script><script src="/canonical-integrity.js?v=20260910-1"></'+'script><script src="/author-practice-hotfix.js?v=20260910-1"></'+'script></body>');
     document.open();
     document.write(html);
     document.close();
